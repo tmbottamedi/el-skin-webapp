954d36d660b3d00fb43219cc2d3556c5b5ebae80
Tiago Mendes Bottamedi <tmbottamedi@mppr.mp.br>
2025-07-10 15:06:16 -0300

    Implementação do Carousel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Header from "components/Header";
 import ProductShowcase from "components/ProductShowcase";
 import Footer from "components/Footer";
+import Carousel from "components/Carousel";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Carousel />
       <ProductShowcase />
       <Footer />
     </>
