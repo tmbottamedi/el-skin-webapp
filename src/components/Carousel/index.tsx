b46d3534fa9e7c248f80dd6080699510b6b6471a
Tiago Mendes Bottamedi <tmbottamedi@mppr.mp.br>
2025-07-15 15:18:43 -0300

    Implementação do Carousel sem Swiper

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,77 +1,121 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import image1 from "assets/carousel1.png";
-import image2 from "assets/carousel2.png";
-import image3 from "assets/carousel3.png";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/effect-fade";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Img1 from "assets/carousel1.png";
+import Img2 from "assets/carousel2.png";
+import Img3 from "assets/carousel3.png";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import "./Carousel.css";
+import { useEffect, useState } from "react";
 
-const slideData = [
-  {
-    image: image1,
-    title: "anti-age",
-    subtitle: "use o cupom ANTIAGE15 e ganhe 15% de desconto",
-    callToAction: {
-      text: "comprar agora",
-      link: "/linha-corporal",
-    },
-  },
-  {
-    image: image2,
-    title: "facial",
-    subtitle: "para uma pele radiante e saudável",
-    callToAction: {
-      text: "descubra",
-      link: "/linha-facial",
-    },
-  },
-  {
-    image: image3,
-    title: "corporal",
-    subtitle: "com benefícios além da hidratação",
-    callToAction: {
-      text: "comprar agora",
-      link: "/linha-corporal",
-    },
-  },
-];
+interface ICarouselItem {
+  subtitle: string;
+  title: string;
+  description: string;
+  backgroundImage: string;
+}
 
-export default function Carousel() {
+function Carousel() {
+  const items: ICarouselItem[] = [
+    {
+      subtitle: "confira nossa linha",
+      title: "corporal",
+      description: "com benefícios além da hidratação",
+      backgroundImage: Img1,
+    },
+    {
+      subtitle: "toda linha",
+      title: "anti-age",
+      description: "use o cupom ANTIAGE15",
+      backgroundImage: Img2,
+    },
+    {
+      subtitle: "",
+      title: "kits incríveis",
+      description: "até 50% OFF",
+      backgroundImage: Img3,
+    },
+  ];
+
+  const [idxItemAtual, setIdxItemAtual] = useState(0);
+
+  function previousItem() {
+    setIdxItemAtual((prevIdx) =>
+      prevIdx === 0 ? items.length - 1 : prevIdx - 1
+    );
+  }
+
+  function nextItem() {
+    setIdxItemAtual((prevIdx) =>
+      prevIdx === items.length - 1 ? 0 : prevIdx + 1
+    );
+  }
+
+  useEffect(() => {
+    console.log("criou o interval....");
+    const timer = setInterval(() => {
+      console.log("ciclou o elemento....");
+      setIdxItemAtual((prevIdxItemAtual) => {
+        return (prevIdxItemAtual + 1) % items.length;
+      });
+    }, 3000);
+
+    return () => {
+      console.log("limpou o interval....");
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      slidesPerView={1}
-      spaceBetween={0}
-      loop={true}
-      navigation={true}
-      pagination={{ clickable: true }}
-      autoplay={{
-        delay: 10000,
-        disableOnInteraction: false,
+    <section
+      className="carouselSection"
+      style={{
+        backgroundImage: `url(${items[idxItemAtual].backgroundImage})`,
       }}
-      className="carousel-container"
     >
-      {slideData.map((slide) => (
-        <SwiperSlide
-          key={slide.image}
-          className="carousel-slide"
-          style={{ backgroundImage: `url(${slide.image})` }}
-        >
-          <div className="slide-content">
-            <p>confira nossa linha</p>
-            <h1>{slide.title}</h1>
-            <p>{slide.subtitle}</p>
-            <a href={slide.callToAction.link} className="cta-button">
-              {slide.callToAction.text} &rarr;
-            </a>
+      <div className="carouselContainer">
+        <div className="carouselContent">
+          <button
+            className="carouselNavButton"
+            aria-label="Voltar"
+            onClick={previousItem}
+          >
+            <FontAwesomeIcon
+              width="60"
+              height="24"
+              icon={faAngleLeft}
+              style={{ color: "white" }}
+            />
+          </button>
+
+          <div className="carouselText">
+            <span className="carouselSubtitle">
+              {items[idxItemAtual].subtitle}
+            </span>
+            <h1 className="carouselTitle">{items[idxItemAtual].title}</h1>
+            <p className="carouselDescription">
+              {items[idxItemAtual].description}
+            </p>
+            <button className="carouselCtaButton">
+              comprar agora
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
           </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+
+          <button
+            className="carouselNavButton"
+            aria-label="Próximo"
+            onClick={nextItem}
+          >
+            <FontAwesomeIcon
+              width="60"
+              height="24"
+              icon={faAngleRight}
+              style={{ color: "white" }}
+            />
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default Carousel;
